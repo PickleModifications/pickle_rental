e521a579bc2215adee31f52e8467183963445a6b
Aecy <devsnake@hotmail.com>
2023-03-03 09:40:38 +0100

    add(translations): french & deutsch

---
+++ locales/translations/de.lua
@@ -0,0 +1,12 @@
+Language["de"] = {
+    marker_interact = "[E] - Fahrzeug mieten",
+    marker_return = "[E] - Fahrzeug zurückgeben",
+    menu_price = "Mietpreis: $%s",
+    menu_no_vehicles = "Sie haben hier keinen Zugang zu Fahrzeugen.",
+    return_not_own = "Sie können dieses Fahrzeug nicht zurückgeben, da Sie es nicht gemietet haben.",
+    return_success = "Sie haben dieses Fahrzeug für $%s zurückgegeben.",
+    return_not_exist = "Dieses Fahrzeug existiert nicht.",
+    spawn_success = "Sie mieten jetzt dieses Fahrzeug und haben $%s hinterlegt.",
+    spawn_failed = "Das Fahrzeug konnte nicht erschaffen werden, deshalb haben Sie $%s zurückerstattet bekommen.",
+    spawn_not_afford = "Sie benötigen $%s mehr, um dieses Fahrzeug zu mieten.",
+}
